7c5dd398fe7cd8d52c7ea459e62d542b1f910589
f s <f s>
2016-01-08 21:11:11 -1000

    Adding first four functions

---
+++ functions_2.js
@@ -0,0 +1,23 @@
+function youGetTaco(action){
+	return action + " TACOS";
+}
+
+function isNumberGreaterThan(first, second){
+	return (first>second);
+}
+
+function isTrue(val){
+	return val;
+}
+
+function isFalse(val){
+	return val;
+}
+
+
+
+
+
+
+console.log(youGetTaco("eat"));
+console.log(isNumberGreaterThan(3,10));
