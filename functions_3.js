6c230f753721bd9ebaf2a410e469ba3c3ef55513
f s <f s>
2016-01-11 19:56:27 -1000

    Adding functions_3.js which is used to test Arrays

---
+++ functions_3.js
@@ -0,0 +1,11 @@
+var food=["cheeseburger","taco","sushi","gyudon","chili"];
+var foods=[["cheeseburger","taco"],["sushi","gyudon","chili"]];
+
+console.log(food[0,3]);
+console.log(food[1,3]);
+console.log(food["q",1,2,0,5,4]);
+
+console.log(foods[1][1]);
+
+
+
